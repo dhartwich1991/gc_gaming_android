735808a7c4549fe953fa3c3ce7cc7faea7dacd76
Daniel Hartwich <daniel.hartwich@xing.com>
2014-12-28 03:21:24 +0100

    clean up available raids task

--- app/src/main/java/com/jdapplications/gcgaming/tasks/AvailableRaidsTask.java
+++ app/src/main/java/com/jdapplications/gcgaming/tasks/AvailableRaidsTask.java
@@ -6,21 +6,15 @@ import com.jdapplications.gcgaming.listener.OnAsyncResultListener;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
-import org.apache.http.NameValuePair;
 import org.apache.http.StatusLine;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by danielhartwich on 12/19/14.
